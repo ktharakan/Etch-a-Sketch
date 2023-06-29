447eb245da01c89934c21ae6b773785257fb395d
Kevin Tharakan <ktharakan04@gmail.com>
2023-06-29 14:57:20 -0400

    grid drawing and website design

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector(".container");
+const colour = "red";
+let isDrawing = false; // Variable to track if the mouse button is held down
 
 function genGrid(v) {
   grid.style.gridTemplateColumns = `repeat(${v}, 1fr)`;
@@ -7,8 +9,25 @@ function genGrid(v) {
   for (let i = 0; i < v * v; i++) {
     const gridElement = document.createElement("div");
     gridElement.classList.add("gridElement");
+    gridElement.addEventListener("mousedown", startDrawing);
+    gridElement.addEventListener("mouseover", draw);
+    gridElement.addEventListener("mouseup", stopDrawing);
     grid.appendChild(gridElement);
   }
 }
 
-genGrid();
+function startDrawing() {
+  isDrawing = true;
+}
+
+function draw(e) {
+  if (isDrawing) {
+    e.target.style.backgroundColor = colour;
+  }
+}
+
+function stopDrawing() {
+  isDrawing = false;
+}
+
+genGrid(60);
